05cb09ab8ec187ff4ee555fdbb2240f07d672942
Sharon Urban <urban_sharon@hotmail.com>
2016-09-30 07:33:16 +0300

    using weak-references in WidgetCache

--- Wallet/Widgets/WidgetCache.cs
+++ Wallet/Widgets/WidgetCache.cs
@@ -8,7 +8,7 @@ namespace Wallet
 	{
 		public delegate T Factory<T>() where T : Widget;
 
-		private IDictionary<Type, Widget> dictionary = new Dictionary<Type, Widget>();
+		private IDictionary<T, WeakReference<T>> dictionary = new Dictionary<T, WeakReference<T>>();
 
 		public T Get<T>(Factory<T> factory) where T : Widget {
 			if (!Contains<T>()) {
@@ -23,11 +23,15 @@ namespace Wallet
 		}
 
 		private void Put<T>(Widget value) where T : Widget {
-			dictionary [typeof(T)] = value;
+			dictionary [typeof(T)] = new WeakReference<Widget>(value);
 		}
 
 		private T Get<T>() where T : Widget {
-			return (T)dictionary [typeof(T)];
+			T t;
+
+			dictionary [typeof(T)].TryGetTarget (out t);
+
+			return t;
 		}
 
 	}
