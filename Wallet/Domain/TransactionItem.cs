ac51dc219a8052a4180081a4550fe3974ba23c02
Sharon <zen.dev2016@gmail.com>
2016-10-05 15:00:07 +0300

    refactoring notebook usage

--- Wallet/Domain/TransactionItem.cs
+++ Wallet/Domain/TransactionItem.cs
@@ -19,6 +19,12 @@ namespace Wallet.Domain
 		public String To { get; set; }
 		public String Id { get; set; }
 		public Decimal Fee { get; set; }
+		public TransactionItem PreviousTransactionItem { get; set; }
+		public Decimal RunningBalance { get 
+			{ 
+				return (Decimal)0.99;
+			}
+		}
 
 		public TransactionItem(Decimal Amount, DirectionEnum Direction, CurrencyEnum Currency, DateTime Date, String To, String Id, Decimal Fee) {
 			this.Amount = Amount;
@@ -31,4 +37,3 @@ namespace Wallet.Domain
 		}
 	}
 }
-
