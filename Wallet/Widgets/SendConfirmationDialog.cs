6dc3f8a79ced7b2a2b4c9f59c6658ae245a786d8
Sharon Urban <urban_sharon@hotmail.com>
2016-10-11 13:20:47 +0300

    wip

---
+++ Wallet/Widgets/SendConfirmationDialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gtk;
+
+namespace Wallet
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public partial class SendConfirmationDialog : DialogBase
+	{
+		public SendConfirmationDialog (ISendDialogView sendDialogView)
+		{
+			this.Build ();
+
+			eventbox1.ButtonReleaseEvent += (object o, ButtonReleaseEventArgs args) => 
+			{
+				CloseDialog();
+				sendDialogView.Cofirm();
+			};
+		}
+	}
+}
+
