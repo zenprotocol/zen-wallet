f9694dbfe8220bdb8dcdebeca360ec4550ab8b71
Nathan Cook <nathan.cook@gmail.com>
2016-10-13 20:39:13 +0300

    Open consensus branch

---
+++ Consensus/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace Consensus
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("Consensus")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("topynate")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
