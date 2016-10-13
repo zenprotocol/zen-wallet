d137382866fe1194ff36a299603ca93ee47887d1
Nathan Cook <nathan.cook@gmail.com>
2016-10-13 23:04:16 +0300

    rough intermediate

--- Consensus/Validate.fs
+++ Consensus/Validate.fs
@@ -1,5 +1,18 @@
-﻿namespace Consensus
+﻿module Consensus.Validate
 
-module Validate
+type Output =
+    PKOutput | ContractOutput | HighVOutput
 
-//let validateTx context transaction =
+type Witness = uint8 list
+
+type PayToPKH = {inputs: PKOutput list, outputs: Output list, witnesses: Witness list}
+
+type Transaction =
+    PayToPKH
+    | PayToCH of {callingInput: CallContractOutput, otherInputs: ContractOutput, witnesses: Witness list}
+    | HighVTx of {version: uint8, inputs: HighVOutput list, outputs: Output list, witnesses: Witness list}
+
+let validateTx context transaction = match transaction with
+                                     | PayToPKH tx -> validatePayToPKH context tx
+                                     | PayToCH tx -> validatePayToCH context tx
+
