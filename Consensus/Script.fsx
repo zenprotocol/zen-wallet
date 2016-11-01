3a71a5251524b953b84de0e1f2a6f88742e63e77
Nathan Cook <nathan.cook@gmail.com>
2016-11-01 18:48:36 +0200

    intermediate of msgserialize

--- Consensus/Script.fsx
+++ Consensus/Script.fsx
@@ -1,6 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Validate.fs"
-open Consensus
+#load "Types.fs"
+#load "Serialization.fs"
+open Consensus.Types
+open Consensus.Serialization
 
