bf502d23afd2f8652701603cdb951337d0915e24
Nathan Cook <nathan.cook@gmail.com>
2016-11-08 19:11:27 +0200

    Intermediate commit adding Tree type, catamorphism

---
+++ Consensus/Tree.fs
@@ -0,0 +1,18 @@
+﻿module Consensus.Tree
+
+type Tree<'LeafData,'INodeData> =
+    | Leaf of 'LeafData
+    | Branch of 'INodeData * Lazy<Tree<'LeafData,'INodeData>> * Lazy<Tree<'LeafData,'INodeData>>
+
+let rec cata fLeaf fBranch (tree : Tree<'LeafData, 'INodeData>) : 'r =
+    let recurse = cata fLeaf fBranch
+    match tree with
+    | Leaf leafData -> fLeaf leafData
+    | Branch (branchData, lazyTreeL, lazyTreeR) ->
+        let lazyRecurse : Lazy<'t> -> Lazy<'r> = fun lTree -> lazy(recurse <| lTree.Force())
+        fBranch branchData <|| (lazyRecurse lazyTreeL, lazyRecurse lazyTreeR)
+
+let map fLeaf fBranch =
+    cata (Leaf << fLeaf)
+         (fun branchData lazyTreeL lazyTreeR ->
+             Branch (fBranch branchData, lazyTreeL, lazyTreeR))
