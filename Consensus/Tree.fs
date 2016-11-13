33816b7319cf8774521709f50e973eb93b97ebf3
Nathan Cook <nathan.cook@gmail.com>
2016-11-13 16:34:52 +0200

    Merkle roots for OptTrees

--- Consensus/Tree.fs
+++ Consensus/Tree.fs
@@ -115,71 +115,9 @@ let complete (s:seq<'T>) =
     let stripLocation = map (fun v -> v.data) (fun v -> v.data)
     addLocation <| locHeight treeWithoutLoc <| stripLocation treeWithoutLoc
 
-
-
-//let fromSeq (s:seq<'T>) =
-//    let toLeaf = Seq.map (Leaf << Some)
-//    let rec parent stack a =
-//        match stack, a with
-//        | y::tl, x when height y = height x -> parent tl (Branch ((height x) + 1, lazy(y), lazy(x)))
-//        | _, _ -> a::stack
-//    let rec withEmptyNodes l =
-//        match l with
-//        | [] -> empty()
-//        | x::[] -> x
-//        | x::y::tl when height x = height y -> withEmptyNodes <| (Branch ((height x) + 1, lazy(y), lazy(x))) :: tl
-//        | x::tl -> withEmptyNodes <| (Branch ((height x) + 1, lazy(emptyN (height x)), lazy(x))) :: tl
-//    withEmptyNodes << Seq.fold parent [] << toLeaf <| s
-
-//let empty = fun () -> Leaf None
-
-//let emptyN<'T> =
-//    let emptySeq =
-//        Seq.unfold
-//        <| fun (n,empN) ->
-//            let nextEmp = Branch (n+1, lazy(empN), lazy(empN))
-//            Some (empN,(n+1,nextEmp))
-//        <| ((0, empty()) : int * LazyTree<'T option, _>)
-//        |> Seq.cache
-//    fun n -> Seq.item n emptySeq
-
-//let height tree = match tree with
-//                  | Leaf _ -> 0
-//                  | Branch (n, _, _) -> n
-
-//type LocBranchData = {height:int32; loc:uint32}
-//type LocLeafData<'T> = {leafData:'T; loc:uint32}
-
-//let addLoc : (LazyTree<'T,int32> -> LazyTree<LocLeafData<'T>,LocBranchData>) =
-//    let right loc height = loc &&& (1u <<< (height - 1))
-//    let rec innerAddLoc : (uint32 -> LazyTree<'T,int32> -> LazyTree<LocLeafData<'T>,LocBranchData>) =
-//        fun loc tree ->
-//            match tree with
-//            | Leaf v -> Leaf {leafData=v; loc=loc}
-//            | Branch (h, lTreeL, lTreeR) ->
-//                Branch <|
-//                (
-//                {height=h;loc=loc},
-//                lazy(innerAddLoc loc <| lTreeL.Force()),
-//                lazy(innerAddLoc (right loc h) <| lTreeR.Force())
-//                )
-//    fun tree -> innerAddLoc 0u tree
-
-//let heightLoc tree = match tree with
-//                     | Leaf _ -> 0
-//                     | Branch ({height=n},_,_) -> n
-
-
-//type Tree<'L,'B> =
-//    | Leaf of 'L
-//    | Branch of data : 'B * left : Tree<'L,'B> option * right : Tree<'L,'B> option
-
-//let rec cata fLeaf fBranch (tree : Tree<'L,'B>) =
-//    let recurse = cata fLeaf fBranch
-//    match tree with
-//    | Leaf leafData -> fLeaf leafData
-//    | Branch (branchData, leftTreeOpt, rightTreeOpt) ->
-//        fBranch branchData
-//        <| Option.map recurse leftTreeOpt
-//        <| Option.map recurse rightTreeOpt
-
+let normalize<'L,'B> =
+    let fBranch (brData:LocData<'B>) tL tR =
+        match tL, tR with
+        | Leaf {data=None:'L option}, Leaf {data=None} -> Leaf {data=None; location=brData.location}
+        | _ , _ -> Branch (brData, tL, tR)
+    cata (Leaf << id) fBranch
