a709a1d5e2407aad4c01d256a51f16ce6d64e3ce
Nathan Cook <nathan.cook@gmail.com>
2016-11-13 18:20:22 +0200

    Tree location in bool array

---
+++ Consensus/SparseMerkleTree.fs
@@ -0,0 +1,15 @@
+﻿module Consensus.SparseMerkleTree
+
+
+open MsgPack
+open MsgPack.Serialization
+
+open Consensus.Types
+open Consensus.Serialization
+
+open Consensus.Tree
+open Consensus.Merkle
+
+let zeroLoc = Array.zeroCreate<bool> 256
+
+//let emptyTree = Branch
