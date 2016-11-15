e39521981d77e72531cd33b4165edf93e0a665a3
Sharon Urban <urban_sharon@hotmail.com>
2016-11-15 16:50:30 +0200

    minor fix

---
+++ Store/StoredItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Store
+{
+	public class StoredItem<T>
+	{
+		public T Value { get; private set; }
+		public byte[] Key { get; private set; }
+		public byte[] Data { get; private set; }
+
+		public StoredItem(T value, byte[] key, byte[] data)
+		{
+			Value = value;
+			Key = key;
+			Data = data;
+		}
+	}
+}
---
+++ Store/TransactionContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Store
+{
+	public class TransactionContext
+	{
+		public TransactionContext()
+		{
+		}
+	}
+}
