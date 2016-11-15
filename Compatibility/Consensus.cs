548675faeeab3bb9eb269c041e4b264df1ba87ea
Sharon <zen.dev2016@gmail.com>
2016-11-15 13:08:37 +0200

    generic store

---
+++ Compatibility/Consensus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Consensus;
+using Microsoft.FSharp.Collections;
+
+namespace Compatibility
+{
+	class Consensus
+	{
+		public static void start()
+		{
+			byte[] hashed = Merkle.transactionHasher.Invoke(GetNewTransaction());
+			Console.WriteLine(Encoding.ASCII.GetString(hashed));
+			Console.ReadLine ();
+		}
+
+		private static Types.Transaction GetNewTransaction()
+		{
+			var endpoints = new List<Types.Outpoint>();
+			var outputs = new List<Types.Output>();
+			var hashes = new List<byte[]>();
+
+			endpoints.Add(new Types.Outpoint(new byte[] { 0x34 }, 222));
+
+			Types.Transaction transaction =
+				new Types.Transaction(1,
+					ListModule.OfSeq(endpoints),
+					ListModule.OfSeq(hashes),
+					ListModule.OfSeq(outputs),
+					null);
+
+			return transaction;
+		}
+	}
+}
---
+++ Store/TransactionStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Consensus;
+using System.IO;
+using System.Linq;
+
+namespace Store
+{
+	public class TransactionStore : Store<Types.Transaction>
+	{
+		public TransactionStore(string dbName, string tableName) : base(dbName, "tx-" + tableName)
+		{
+		}
+
+		protected override StoredItem<Types.Transaction> Wrap(Types.Transaction item)
+		{
+			var data = Merkle.serialize<Types.Transaction>(item);
+			var key = Merkle.transactionHasher.Invoke(item);
+
+			return new StoredItem<Types.Transaction>(item, key, data);
+		}
+
+		protected override Types.Transaction FromBytes(byte[] data, byte[] key)
+		{
+			//TODO: encap unpacking in Consensus, so referencing MsgPack would becode unnecessary 
+			return Serialization.context.GetSerializer<Types.Transaction>().UnpackSingleObject(data);
+		}
+	}
+}
