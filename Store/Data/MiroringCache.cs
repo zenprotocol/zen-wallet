b39bbfb0fbb530ea253c3decc16f7b3f4895f40f
Sharon <zen.dev2016@gmail.com>
2016-11-17 12:57:05 +0200

    intermediate blockchain work

---
+++ Store/Data/MiroringCache.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using DBreeze;
+
+//namespace Store
+//{
+//	public abstract class MiroringCache<T> : Store<T> where T : class
+//	{
+//		protected MiroringCache(string tableName) : base(tableName)
+//		{
+			
+//		}
+//	}
+//}
