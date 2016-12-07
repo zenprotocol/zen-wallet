8bfd589c1ef5a34a7a92878370c5009025646005
Sharon <zen.dev2016@gmail.com>
2016-12-07 17:44:41 +0200

    nbitcoin cleanup

---
+++ Wallet/Widgets/ReceiveDialog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Wallet
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ReceiveDialog : Gtk.Bin
+	{
+		public ReceiveDialog()
+		{
+			this.Build();
+		}
+	}
+}
