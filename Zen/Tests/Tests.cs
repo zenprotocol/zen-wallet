3f396b3621815608c29dd7e3b3ea3809cc91a610
Sharon <urban_sharon@hotmail.com>
2017-02-01 17:27:31 +0200

    added "tests" menu

--- Zen/Tests/Tests.cs
+++ Zen/Tests/Tests.cs
@@ -1,13 +1,28 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Linq;
+using Infrastructure;
+
 namespace Zen
 {
 	[TestFixture()]
 	public class Tests
 	{
 		[Test()]
-		public void TestCase()
+		public void CanAquireGenesisOutputs()
 		{
+			App app = new App();
+
+			app.Settings.Mode = Settings.AppModeEnum.GUI;
+			app.Init();
+
+			app.AddGenesisBlock();
+
+			JsonLoader<Outputs>.Instance.Value.Values.ForEach(o => app.AddKey(o.Key));
+
+			app.ImportWallet();
+
+			app.Start();
 		}
 	}
 }
