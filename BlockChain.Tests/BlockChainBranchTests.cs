4d56c33261c2be8ecb7e10e2a1a6d05b6426e096
Sharon <urban_sharon@hotmail.com>
2017-02-05 02:23:51 +0200

    * BlockChainBranchTests.cs: new tests project
    * packages.config:
    * BlockChainSimpleTests.cs:
    * BlockChainTransactionRelocationTests.cs:

--- BlockChain.Tests/BlockChainBranchTests.cs
+++ BlockChain.Tests/BlockChainBranchTests.cs
@@ -1,42 +1,32 @@
-//using System;
-//using Consensus;
-//using BlockChain.Store;
-//using Store;
-//using Infrastructure;
-//using System.Text;
-//using System.Collections.Generic;
-//using Microsoft.FSharp.Collections;
-//using System.Linq;
-//using BlockChain.Data;
-//using NUnit.Framework;
-//using System.IO;
+using BlockChain.Store;
+using NUnit.Framework;
+using Infrastructure.Testing;
+namespace BlockChain.Tests
+{
+	[TestFixture()]
+	public class BlockChainBranchTests : BlockChainTestsBase
+	{
+		[Test()]
+		public void ShouldDetectBranch()
+		{
+			var block1 = _GenesisBlock.Child();
+			var block2 = _GenesisBlock.Child();
 
-//namespace BlockChain.Tests
-//{
-//	[TestFixture()]
-//	public class BlockChainBranchTests : BlockChainTestsBase
-//	{
-//		[Test()]
-//		public void ShouldDetectBranch()
-//		{
-//			var block1 = GetBlock(_GenesisBlock);
-//			var block2 = GetBlock(_GenesisBlock);
+			Assert.That(_BlockChain.HandleNewBlock(_GenesisBlock), Is.EqualTo(AddBk.Result.Added));
+			Assert.That(_BlockChain.HandleNewBlock(block1), Is.EqualTo(AddBk.Result.Added));
+			Assert.That(_BlockChain.HandleNewBlock(block2), Is.EqualTo(AddBk.Result.Added));
 
-//			Assert.That(_BlockChain.HandleNewBlock(_GenesisBlock), Is.EqualTo(AddBk.Result.Added));
-//			Assert.That(_BlockChain.HandleNewBlock(block1.Value), Is.EqualTo(AddBk.Result.Added));
-//			Assert.That(_BlockChain.HandleNewBlock(block2.Value), Is.EqualTo(AddBk.Result.Added));
+			Assert.That(Location(_GenesisBlock), Is.EqualTo(LocationEnum.Main));
+			Assert.That(Location(block1), Is.EqualTo(LocationEnum.Main));
 
-//			Assert.That(Location(_GenesisBlock), Is.EqualTo(LocationEnum.Main));
-//			Assert.That(Location(block1), Is.EqualTo(LocationEnum.Main));
+			// detect branching
+			Assert.That(Location(block2), Is.EqualTo(LocationEnum.Branch));
 
-//			// detect branching
-//			Assert.That(Location(block2), Is.EqualTo(LocationEnum.Branch));
-
-//			// detect branch child
-//			var block3 = GetBlock(block2);
-//			Assert.That(_BlockChain.HandleNewBlock(block3.Value), Is.EqualTo(AddBk.Result.Added));
-//			Assert.That(Location(block3), Is.EqualTo(LocationEnum.Main));
-//		}
-//	}
+			// detect branch child
+			var block3 = block2.Child();
+			Assert.That(_BlockChain.HandleNewBlock(block3), Is.EqualTo(AddBk.Result.Added));
+			Assert.That(Location(block3), Is.EqualTo(LocationEnum.Main));
+		}
+	}
 	
-//}
+}
