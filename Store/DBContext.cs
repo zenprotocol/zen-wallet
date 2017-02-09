7fa78f6e8cb17d68ae45ae82b257907e91e50fe7
Sharon <urban_sharon@hotmail.com>
2017-02-09 17:22:37 +0200

    refactoring

--- Store/DBContext.cs
+++ Store/DBContext.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Threading;
 using DBreeze;
 
 namespace Store
 {
 	public class DBContext : IDisposable
 	{
+		private System.Collections.Generic.List<TransactionContext> _List = new System.Collections.Generic.List<TransactionContext>();
 		private DBreezeEngine _Engine;
+		private ManualResetEvent _Event = new ManualResetEvent(false);
 
 		public DBContext(string dbName)
 		{
@@ -14,11 +17,25 @@ namespace Store
 
 		public TransactionContext GetTransactionContext()
 		{
-			return new TransactionContext(_Engine.GetTransaction());
+			var t = new TransactionContext(this, _Engine.GetTransaction());
+			_List.Add(t);
+			_Event.Reset();
+			return t;
+		}
+
+		public void Remove(TransactionContext transaction)
+		{
+			_List.Remove(transaction);
+
+			if (_List.Count == 0)
+			{
+				_Event.Set();
+			}
 		}
 
 		public void Dispose()
 		{
+			_Event.WaitOne();
 			_Engine.Dispose();
 		}
 	}
