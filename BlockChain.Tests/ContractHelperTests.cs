ce702b4a15599eb02547db399e701b01f68427a4
Sharon <urban_sharon@hotmail.com>
2017-02-13 20:52:57 +0200

    * ACSTests.cs: ACL tests
    * ContractTests.cs:
    * ContractIneractionTests.cs:
    * BlockChainContractTestsBase.cs:
    
    * BlockChainSimpleTests.cs: added test for inter-dependent txs in a
      block
    
    * ContractHelperTests.cs: tests helper

--- BlockChain.Tests/ContractHelperTests.cs
+++ BlockChain.Tests/ContractHelperTests.cs
@@ -14,13 +14,6 @@ namespace BlockChain
 		byte[] compiledContract;
 		Types.Transaction contractCreatedTransaction;
 		List<Types.Output> outputs = new List<Types.Output>();
-	//	List<Types.Outpoint> inputs = new List<Types.Outpoint>();
-
-		//[OneTimeSetUp]
-		//public void OneTimeSetUp()
-		//{
-		//	base.OneTimeSetUp();
-		//}
 
 		[Test, Order(1)]
 		public void ShouldCompile()
@@ -66,82 +59,10 @@ let run (context : ContractContext, witnesses: Witness list, outputs: Output lis
 			Assert.That(ContractHelper.Execute(compiledContract, out contractCreatedTransaction, new ContractArgs()
 			{
 				context = new Types.ContractContext(compiledContract, new FSharpMap<Types.Outpoint, Types.Output>(utxos)),
-		//		inputs = inputs,
 				witnesses = new List<byte[]>(),
 				outputs = outputs,
 				option = Types.ExtendedContract.NewContract(new Types.Contract(new byte[] { },new byte[] { },new byte[] { }))
 			}), "Should execute", Is.True);
 		}
-
-		[Test, Order(3)]
-		public void ShouldValidate()
-		{
-			CollectionAssert.AreEqual(contractCreatedTransaction.outputs, outputs);
-		}
-
-		[Test, Order(4)]
-		public void ShouldValidateInMempool()
-		{
-			using (var dbTx = _BlockChain.GetDBTransaction())
-			{
-				_BlockChain.ActiveContractSet.Add(dbTx, compiledContract, 1000);
-				dbTx.Commit();
-			}
-
-			Assert.That(_BlockChain.HandleTransaction(contractCreatedTransaction), "Should add block", Is.True);
-		}
-
-		[Test, Order(5)]
-		public void ShouldValidateInBlock()
-		{
-			var bk = _GenesisBlock.Child().AddTx(contractCreatedTransaction);
-
-			Assert.That(_BlockChain.HandleBlock(bk), "Should add block", Is.False);
-		}
-
-		[Test, Order(6)]
-		public void ShouldActivateContract()
-		{
-		}
-
-		[Test, Order(7)]
-		public void ShouldExtendContract()
-		{
-			using (var dbTx = _BlockChain.GetDBTransaction())
-			{
-				_BlockChain.ActiveContractSet.Add(dbTx, compiledContract, 1000);
-				dbTx.Commit();
-			}
-
-			var output = Utils.GetContractOutput(compiledContract, null, null, 12);
-			var tx = Utils.GetTx().AddOutput(output);
-
-			////
-			var bk = _GenesisBlock.Child().AddTx(contractCreatedTransaction);
-
-			Assert.That(_BlockChain.HandleTransaction(tx), "Should accept tx", Is.True);
-		}
-
-
-		[Test, Order(7)]
-		public void ShouldExtendMempoolContract()
-		{
-			var output = Utils.GetContractOutput(compiledContract, null, null, 12);
-			var tx = Utils.GetTx().AddOutput(output);
-
-			////
-			var bk = _GenesisBlock.Child().AddTx(contractCreatedTransaction);
-
-			Assert.That(_BlockChain.HandleTransaction(tx), "Should accept tx", Is.True);
-		}
-
-		[Test, Order(8)]
-		public void ShouldNotExtendInvalidContract()
-		{
-
-		}
-
-		//TODO: test peer banning
-
 	}
 }
