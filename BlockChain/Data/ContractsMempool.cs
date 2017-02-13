3f4fc8d5412f33dfa56a7d2ce293c8ed3721473d
Sharon <urban_sharon@hotmail.com>
2017-02-13 18:03:24 +0200

    * BlockChain.cs: * HashDictionary.cs: * ContractsMempool.cs: contracts mempool update [not finished]
    * ContractHelper.cs: contract compilation and execution semantics

--- BlockChain/Data/ContractsMempool.cs
+++ BlockChain/Data/ContractsMempool.cs
@@ -4,19 +4,36 @@ namespace BlockChain.Data
 {
 	public class ContractsMempoolItem
 	{
-		byte[] Hash { get; set; }
-		string AssemblyFile { get; set; }
-		byte[] CostFn { get; set; }
-		ulong Cost { get; set; }
-		HashSet LastBlock { get; set; }
+		public byte[] Hash { get; set; }
+		public string AssemblyFile { get; set; }
+		public byte[] CostFn { get; set; }
+		public ulong Cost { get; set; }
+		public HashSet LastBlock { get; set; }
 	}
 
 	public class ContractsMempool
 	{
-		private readonly HashDictionary<ContractsMempoolItem> _Contracts;
+		private readonly HashDictionary<ContractsMempoolItem> _Contracts = new HashDictionary<ContractsMempoolItem>();
 
-		public ContractsMempool()
+		public void Add(ContractsMempoolItem item)
 		{
+			_Contracts[item.Hash] = item;
+		}
+
+		public void RemoveTx(byte[] txHash)
+		{
+			foreach (var item in _Contracts)
+			{
+				if (item.Value.LastBlock.Contains(txHash))
+				{
+					item.Value.LastBlock.Remove(txHash);
+				}
+
+				if (item.Value.LastBlock.Count == 0)
+				{
+					_Contracts.Remove(item.Key); //TODO: messing with iterator?
+				}
+			}
 		}
 	}
 }
