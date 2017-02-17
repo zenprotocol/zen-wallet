e55e1984fb9cfad89bb2b4781d6ef3b7561fbce7
Sharon <urban_sharon@hotmail.com>
2017-02-17 17:24:09 +0200

    * BlockChain.cs: added tx outpoint validity check
    * BlockVerificationHelper.cs: added tx outpoint validity check, added
      check for tx existence when process block txs - using utxo's
    
    * DBContext.cs: added lock [tested]
    
    * BlockChainSimpleTests: minor fix
    
    * app.config files: trace configuration fix

---
+++ BlockChain.Tests/MempoolTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Infrastructure.Testing;
+using NUnit.Framework;
+using Wallet.core.Data;
+
+namespace BlockChain
+{
+	public class MempoolTests : BlockChainTestsBase
+	{
+		[Test, Order(1)]
+		public void ShouldUnorphan()
+		{
+			var tx = Utils.GetTx();
+
+			var txOrpan = Utils.GetTx().AddInput(tx, 0, new Key().Address);
+
+			_BlockChain.HandleTransaction(txOrpan);
+			Assert.That(_BlockChain.memPool.OrphanTxPool.ContainsKey(txOrpan.Key()), Is.True, "should be there");
+
+			_BlockChain.HandleTransaction(tx);
+			Assert.That(_BlockChain.memPool.OrphanTxPool.ContainsKey(txOrpan.Key()), Is.True, "should not be there");
+		}
+	}
+}
