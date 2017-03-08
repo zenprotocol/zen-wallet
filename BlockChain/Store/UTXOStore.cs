d40b9c9f1b01ec4901c7f266c114756258c35787
Sharon <urban_sharon@hotmail.com>
2017-03-08 13:15:04 +0200

    cleanup

--- BlockChain/Store/UTXOStore.cs
+++ BlockChain/Store/UTXOStore.cs
@@ -32,40 +32,4 @@ namespace BlockChain.Store
 			return ContainsKey(dbTx, new Types.Outpoint(txHash, index));
 		}
 	}
-
-	//public class UTXOMiroringCache
-	//{
-	//	private readonly UTXOStore _UTXOStore;
-
-	//	public UTXOMiroringCache() 
-	//	{
-	//		_UTXOStore = new UTXOStore();
-	//	}
-	
-	//	private void Put() {
-	//	}
-
-	//	private void Get() {
-	//	}
-
-
-	//	private class UTXOStore : Store<Types.Output>
-	//	{
-	//		public UTXOStore() : base("utxo") { }
-
-	//		protected override StoredItem<Types.Output> Wrap(Types.Output item)
-	//		{
-	//			var data = Merkle.serialize<Types.Output>(item);
-	//			var key = Merkle.outputHasher.Invoke(item);
-
-	//			return new StoredItem<Types.Output>(item, key, data);
-	//		}
-
-	//		protected override Types.Output FromBytes(byte[] data, byte[] key)
-	//		{
-	//			//TODO: encap unpacking in Consensus, so referencing MsgPack would become unnecessary 
-	//			return Serialization.context.GetSerializer<Types.Output>().UnpackSingleObject(data);
-	//		}
-	//	}
-	//}
 }
