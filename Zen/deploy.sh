e0264b853e690b1e62682451e9a4153955dc3d8e
Sharon <urban_sharon@hotmail.com>
2017-03-12 13:41:40 +0200

    cleanup

--- Zen/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -az --no-r bin/Debug/*.* ubuntu@54.218.251.235:/home/ubuntu/Debug4
-
-# rsync -az --no-r bin/Debug/*.* --exclude *.xml ubuntu@54.218.251.235:/home/ubuntu/Debug3
--- Zen/ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh ubuntu@54.218.251.235
