2b8cffca0e935b04c8834408bc86e2e08008b0e1
Sharon <urban_sharon@hotmail.com>
2017-04-13 22:59:05 +0300

    D3 DHTML graph generator for actual blockchain's state (unfinished)

--- Zen/BlockChainDumper.cs
+++ Zen/BlockChainDumper.cs
@@ -1,33 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using BlockChain.Data;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace Zen
 {
 	public enum GraphNodeTypeEnum
 	{
+		Genesis,
 		Block,
 		Tx,
 		Output,
-		Input
+		Outpoint,
+		Input,
+		Missing
 	}
 
 	public class GraphNode
 	{
-		//public string Title { get; set; }
-		//public int Size { get; set; }
-		public GraphNodeTypeEnum GraphNodeTypes { get; set; }
+		public string Title { get; set; }
+		public GraphNodeTypeEnum GraphNodeType { get; set; }
 
-		public GraphNode(GraphNodeTypeEnum graphNodeTypes)
+		public GraphNode(GraphNodeTypeEnum graphNodeType, string title = null)
 		{
-			GraphNodeTypes = graphNodeTypes;
+			GraphNodeType = graphNodeType;
+			Title = title;
 		}
 	}
 
 	public class GraphLink
 	{
-		public GraphNode GraphNode1 { get; set; }
-		public GraphNode GraphNode2 { get; set; }
+		public byte[] GraphNode1 { get; set; }
+		public byte[] GraphNode2 { get; set; }
+		//public GraphNode GraphNode1 { get; set; }
+		//public GraphNode GraphNode2 { get; set; }
 	}
 
 	public class Graph
@@ -40,27 +47,82 @@ namespace Zen
 			GraphNodes = new List<GraphNode>();
 			GraphLinks = new List<GraphLink>();
 		}
+
+		public void Reset()
+		{
+			GraphNodes.Clear();
+			GraphLinks.Clear();
+		}
 	}
 
-	public class EmptyClass
+	public class D3Node
 	{
-		BlockChain.BlockChain _BlockChain;
-		Graph _Graph;
-		HashDictionary<GraphNode> Keys;
+		public string atom { get; set; }
+		public int size { get; set; }
+		public int color { get; set; }
+	}
 
-		public void x()
+	public class D3Link
+	{
+		public int source { get; set; }
+		public int target { get; set; }
+		public int bond { get; set; }
+	}
+
+	public class D3Graph
+	{
+		public List<D3Node> nodes { get; set; }
+		public List<D3Link> links { get; set; }
+
+		public D3Graph()
 		{
+			nodes = new List<D3Node>();
+			links = new List<D3Link>();
+		}
+	}
+
+	public class BlockChainDumper
+	{
+		readonly BlockChain.BlockChain _BlockChain;
+		readonly Graph _Graph = new Graph();
+		readonly HashDictionary<GraphNode> _Keys = new HashDictionary<GraphNode>();
+		readonly HashDictionary<int> _Indexes = new HashDictionary<int>();
+
+		public BlockChainDumper(BlockChain.BlockChain blockChain)
+		{
+			_BlockChain = blockChain;
+		}
+
+		public void Populate()
+		{
+			_Graph.Reset();
+
 			var tip = _BlockChain.Tip;
 
 			if (tip != null)
 			{
 				var bkHash = tip.Key;
 				var bk = tip.Value;
+				byte[] lastKey = null;
 
-				while (bkHash != null)
+				while (bk != null)
 				{
-					Add(bkHash, new GraphNode(GraphNodeTypeEnum.Block);
+					var graphNode = new GraphNode(tip.Key.SequenceEqual(bkHash) ? GraphNodeTypeEnum.Genesis : GraphNodeTypeEnum.Block);
+					Add(bkHash, graphNode);
 
+					if (lastKey != null)
+					{
+						Link(bkHash, lastKey);
+					}
+
+					foreach (var tx in bk.transactions)
+					{
+						var txHash = Consensus.Merkle.transactionHasher.Invoke(tx);
+						AddTx(txHash, tx, TxStateEnum.Confirmed);
+						Link(txHash, bkHash);
+					}
+
+					lastKey = bkHash;
 					bkHash = tip.Value.header.parent;
 					bk = _BlockChain.GetBlock(bkHash);
 				}
@@ -68,14 +130,161 @@ namespace Zen
 
 			foreach (var i in _BlockChain.memPool.TxPool)
 			{
-				Add(i.Key, new GraphNode(GraphNodeTypeEnum.Tx));
+				AddTx(i.Key, Consensus.TransactionValidation.unpoint(i.Value), TxStateEnum.Unconfirmed);
 			}
 		}
 
-		void Add(byte[] key, GraphNode graphNode)
+		void AddTx(byte[] txHash, Consensus.Types.Transaction tx, TxStateEnum txState)
 		{
-			Keys.Add(key, graphNode);
-			_Graph.GraphNodes.Add(graphNode);
+			Add(txHash, new GraphNode(GraphNodeTypeEnum.Tx, txState.ToString()));
+
+			uint i = 0;
+			foreach (var output in tx.outputs)
+			{
+				var outputHash = Consensus.Merkle.outputHasher.Invoke(output);
+				Add(outputHash, new GraphNode(GraphNodeTypeEnum.Output, (output.spend.amount / Math.Pow(10,8)).ToString()), txHash);
+
+				var outpointHash = Consensus.Merkle.outpointHasher.Invoke(new Consensus.Types.Outpoint(txHash, i++));
+				Add(outpointHash, new GraphNode(GraphNodeTypeEnum.Outpoint), txHash);
+				Link(outpointHash, outputHash);
+			}
+
+			foreach (var outpoint in tx.inputs)
+			{
+				Add(Consensus.Merkle.outpointHasher.Invoke(outpoint), new GraphNode(GraphNodeTypeEnum.Input), txHash);
+			}
 		}
-	}
+
+		public string Generate()
+		{
+			var d3Graph = new D3Graph();
+
+			foreach (var graphNode in _Graph.GraphNodes)
+			{
+				d3Graph.nodes.Add(new D3Node()
+				{
+					atom = graphNode.GraphNodeType.ToString() + (graphNode.Title == null ? "" : " (" + graphNode.Title + ")"),
+					color = GetColor(graphNode.GraphNodeType),
+					size = GetSize(graphNode.GraphNodeType)
+				});
+			}
+
+			foreach (var graphLink in _Graph.GraphLinks)
+			{
+				d3Graph.links.Add(new D3Link()
+				{
+					bond = 1,
+					source = _Indexes[graphLink.GraphNode1],
+					target = _Indexes[graphLink.GraphNode2]
+				});
+			}
+
+			return JsonConvert.SerializeObject(d3Graph, Formatting.Indented);
+		}
+
+		void Add(byte[] key, GraphNode graphNode, byte[] parent = null)
+		{
+			if (!_Keys.ContainsKey(key))
+			{
+				_Indexes.Add(key, _Indexes.Count);
+				_Keys.Add(key, graphNode);
+				_Graph.GraphNodes.Add(graphNode);
+			}
+
+			if (parent != null)
+				Link(parent, key);
+		}
+
+		void Link(byte[] ref1, byte[] ref2)
+		{
+			foreach (var _graphLink in _Graph.GraphLinks)
+			{
+				if (_graphLink.GraphNode1.SequenceEqual(ref1) &&
+					_graphLink.GraphNode2.SequenceEqual(ref2) ||
+				   _graphLink.GraphNode1.SequenceEqual(ref2) &&
+					_graphLink.GraphNode2.SequenceEqual(ref1))
+					return;
+			}
+
+			if (!_Keys.ContainsKey(ref1))
+			{
+				Add(ref1, new GraphNode(GraphNodeTypeEnum.Missing));
+			}
+
+			if (!_Keys.ContainsKey(ref2))
+			{
+				Add(ref2, new GraphNode(GraphNodeTypeEnum.Missing));
+			}
+
+			_Graph.GraphLinks.Add(new GraphLink()
+			{
+				GraphNode1 = ref1,
+				GraphNode2 = ref2
+			});
+		}
+
+		//void Link(byte[] ref1, GraphNode ref2)
+		//{
+		//	_Graph.GraphLinks.Add(new GraphLink()
+		//	{
+		//		GraphNode1 = _Keys[ref1],
+		//		GraphNode2 = ref2
+		//	});
+		//}
+
+		//void Link(GraphNode ref1, GraphNode ref2)
+		//{
+		//	_Graph.GraphLinks.Add(new GraphLink()
+		//	{
+		//		GraphNode1 = ref1,
+		//		GraphNode2 = ref2
+		//	});
+		//}
+
+		//void LinkInner(GraphLink graphLink)
+		//{
+		//	foreach (var _graphLink in _Graph.GraphLinks)
+		//	{
+		//		if (_graphLink.GraphNode1 
+		//	}
+		//}
+
+		int GetColor(GraphNodeTypeEnum graphNodeType)
+		{
+			switch (graphNodeType)
+			{
+				case GraphNodeTypeEnum.Genesis:
+					return 6;
+				case GraphNodeTypeEnum.Block:
+					return 1;
+				case GraphNodeTypeEnum.Input:
+					return 2;
+				case GraphNodeTypeEnum.Output:
+					return 3;
+				case GraphNodeTypeEnum.Tx:
+					return 4;
+				default:
+					return 0;
+			}
+		}
+
+		int GetSize(GraphNodeTypeEnum graphNodeType)
+		{
+			switch (graphNodeType)
+			{
+				case GraphNodeTypeEnum.Genesis:
+					return 60;
+				case GraphNodeTypeEnum.Block:
+					return 40;
+				case GraphNodeTypeEnum.Input:
+					return 15;
+				case GraphNodeTypeEnum.Output:
+					return 15;
+				case GraphNodeTypeEnum.Tx:
+					return 30;
+				default:
+					return 5;
+			}
+		}					
+  	}
 }
