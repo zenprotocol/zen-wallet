df5a8b7829003fc9abee8ae61475255c97c7f269
Sharon <urban_sharon@hotmail.com>
2017-04-17 15:00:24 +0300

    graph fixes

--- Zen/BlockChainDumper.cs
+++ Zen/BlockChainDumper.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using BlockChain.Data;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Text;
+using Wallet.core;
+using Consensus;
 
 namespace Zen
 {
 	public enum GraphNodeTypeEnum
 	{
+		UTXOSet,
+		MyWallet,
+		MemPool,
 		Genesis,
 		Block,
 		Tx,
@@ -33,8 +39,6 @@ namespace Zen
 	{
 		public byte[] GraphNode1 { get; set; }
 		public byte[] GraphNode2 { get; set; }
-		//public GraphNode GraphNode1 { get; set; }
-		//public GraphNode GraphNode2 { get; set; }
 	}
 
 	public class Graph
@@ -87,15 +91,30 @@ namespace Zen
 		readonly Graph _Graph = new Graph();
 		readonly HashDictionary<GraphNode> _Keys = new HashDictionary<GraphNode>();
 		readonly HashDictionary<int> _Indexes = new HashDictionary<int>();
+		readonly HashDictionary<byte[]> _Nodes = new HashDictionary<byte[]>();
+		readonly HashDictionary<Types.Transaction> _Txs = new HashDictionary<Types.Transaction>();
+
+		byte[] memPool;
+		byte[] myWallet;
+		byte[] utxoSet;
 
 		public BlockChainDumper(BlockChain.BlockChain blockChain)
 		{
 			_BlockChain = blockChain;
 		}
 
-		public void Populate()
+		public void Populate(WalletManager walletManager, BlockChain.BlockChain blockChain)
 		{
 			_Graph.Reset();
+
+			memPool = Encoding.ASCII.GetBytes(GraphNodeTypeEnum.MemPool.ToString());
+			Add(memPool, new GraphNode(GraphNodeTypeEnum.MemPool));
+
+			myWallet = Encoding.ASCII.GetBytes(GraphNodeTypeEnum.MyWallet.ToString());
+			Add(myWallet, new GraphNode(GraphNodeTypeEnum.MyWallet));
+
+			utxoSet = Encoding.ASCII.GetBytes(GraphNodeTypeEnum.UTXOSet.ToString());
+			Add(utxoSet, new GraphNode(GraphNodeTypeEnum.UTXOSet));
 
 			var tip = _BlockChain.Tip;
 
@@ -107,7 +126,7 @@ namespace Zen
 
 				while (bk != null)
 				{
-					var graphNode = new GraphNode(tip.Key.SequenceEqual(bkHash) ? GraphNodeTypeEnum.Genesis : GraphNodeTypeEnum.Block);
+					var graphNode = new GraphNode(bk.header.parent.Length == 0 ? GraphNodeTypeEnum.Genesis : GraphNodeTypeEnum.Block, "" + bk.header.blockNumber);
 					Add(bkHash, graphNode);
 
 					if (lastKey != null)
@@ -123,7 +142,7 @@ namespace Zen
 					}
 
 					lastKey = bkHash;
-					bkHash = tip.Value.header.parent;
+					bkHash = bk.header.parent;
 					bk = _BlockChain.GetBlock(bkHash);
 				}
 			}
@@ -131,6 +150,65 @@ namespace Zen
 			foreach (var i in _BlockChain.memPool.TxPool)
 			{
 				AddTx(i.Key, Consensus.TransactionValidation.unpoint(i.Value), TxStateEnum.Unconfirmed);
+			}
+
+			LinkTxOutpoints();
+			LinkMyWallet(walletManager);
+			LinkUTXO();
+		}
+
+		void LinkTxOutpoints()
+		{
+			foreach (var tx in _Txs.Values)
+			{
+				foreach (var outpoint in tx.inputs)
+				{
+					var outpointHash = Consensus.Merkle.outpointHasher.Invoke(outpoint);
+
+					if (_Nodes.ContainsKey(outpointHash))
+						Link(outpointHash, _Nodes[outpointHash]);
+				}
+			}
+		}
+
+		void LinkMyWallet(WalletManager walletManager)
+		{
+			var keys = walletManager.GetKeys();
+
+			foreach (var tx in _Txs.Values)
+			{
+				foreach (var output in tx.outputs)
+				{
+					foreach (var key in keys)
+					{
+						if (key.Address.IsMatch(output.@lock))
+						{
+							var outputHash = Consensus.Merkle.outputHasher.Invoke(output);
+
+							Link(outputHash, myWallet);
+						}
+					}
+				}
+			}
+		}
+
+		void LinkUTXO()
+		{
+			HashDictionary<List<Types.Output>> txOutputs;
+			HashDictionary<Types.Transaction> txs;
+			_BlockChain.GetUTXOSet(null, out txOutputs, out txs); // TODO: use linq, return enumerator, remove predicate
+
+			foreach (var tx in _Txs)
+			{
+				if (txs.ContainsKey(tx.Key))
+				{
+					foreach (var output in txOutputs[tx.Key])
+					{
+						var outputHash = Consensus.Merkle.outputHasher.Invoke(output);
+
+						Add(outputHash, new GraphNode(GraphNodeTypeEnum.Output), utxoSet);
+					}
+				}
 			}
 		}
 
@@ -142,17 +220,24 @@ namespace Zen
 			foreach (var output in tx.outputs)
 			{
 				var outputHash = Consensus.Merkle.outputHasher.Invoke(output);
-				Add(outputHash, new GraphNode(GraphNodeTypeEnum.Output, (output.spend.amount / Math.Pow(10,8)).ToString()), txHash);
+				var outputGraphNode = new GraphNode(GraphNodeTypeEnum.Output, (output.spend.amount / Math.Pow(10, 8)).ToString());
+				Add(outputHash, outputGraphNode, txHash);
 
-				var outpointHash = Consensus.Merkle.outpointHasher.Invoke(new Consensus.Types.Outpoint(txHash, i++));
-				Add(outpointHash, new GraphNode(GraphNodeTypeEnum.Outpoint), txHash);
-				Link(outpointHash, outputHash);
+				var outpointHash = Consensus.Merkle.outpointHasher.Invoke(new Consensus.Types.Outpoint(txHash, i));
+				_Nodes[outpointHash] = outputHash;
+				i++;
 			}
 
 			foreach (var outpoint in tx.inputs)
 			{
-				Add(Consensus.Merkle.outpointHasher.Invoke(outpoint), new GraphNode(GraphNodeTypeEnum.Input), txHash);
+				var outpointHash = Consensus.Merkle.outpointHasher.Invoke(outpoint);
+				Add(outpointHash, new GraphNode(GraphNodeTypeEnum.Outpoint), txHash);
 			}
+
+			if (txState == TxStateEnum.Unconfirmed)
+				Link(txHash, memPool);
+
+			_Txs.Add(txHash, tx);
 		}
 
 		public string Generate()
@@ -223,32 +308,6 @@ namespace Zen
 			});
 		}
 
-		//void Link(byte[] ref1, GraphNode ref2)
-		//{
-		//	_Graph.GraphLinks.Add(new GraphLink()
-		//	{
-		//		GraphNode1 = _Keys[ref1],
-		//		GraphNode2 = ref2
-		//	});
-		//}
-
-		//void Link(GraphNode ref1, GraphNode ref2)
-		//{
-		//	_Graph.GraphLinks.Add(new GraphLink()
-		//	{
-		//		GraphNode1 = ref1,
-		//		GraphNode2 = ref2
-		//	});
-		//}
-
-		//void LinkInner(GraphLink graphLink)
-		//{
-		//	foreach (var _graphLink in _Graph.GraphLinks)
-		//	{
-		//		if (_graphLink.GraphNode1 
-		//	}
-		//}
-
 		int GetColor(GraphNodeTypeEnum graphNodeType)
 		{
 			switch (graphNodeType)
@@ -272,6 +331,8 @@ namespace Zen
 		{
 			switch (graphNodeType)
 			{
+				case GraphNodeTypeEnum.UTXOSet:
+				case GraphNodeTypeEnum.MemPool:
 				case GraphNodeTypeEnum.Genesis:
 					return 60;
 				case GraphNodeTypeEnum.Block:
