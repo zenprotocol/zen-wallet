ad8ceee40ce9dac8d8438d12ac532969e01162c4
Sharon Urban <urban_sharon@hotmail.com>
2017-06-14 21:39:39 +0300

    added RPC request message types

--- Zen.RPC.Common/Payloads.cs
+++ Zen.RPC.Common/Payloads.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Consensus;
+using System.Linq;
 
 namespace Zen.RPC.Common
 {
@@ -9,12 +12,28 @@ namespace Zen.RPC.Common
 		{
 			Type = this.GetType();
 		}
+
+		public override string ToString()
+		{
+			return Type.Name;
+		}
 	}
 
 	public class ResultPayload
 	{
 		public bool Success { get; set; }
 		public string Message { get; set; }
+
+		public override string ToString()
+		{
+			var status = Success ? "" : "Failure";
+			var str = $"{this.GetType().Name} {status}";
+
+			if (Message != null)
+				str += $" Message={Message}";
+
+			return str;
+		}
 	}
 
 	public class HelloPayload : BasePayload
@@ -26,7 +45,7 @@ namespace Zen.RPC.Common
 		public HelloResultPayload()
 		{
 			Success = true;
-			Message = "Hello! it's " + DateTime.Now.ToLongTimeString();
+			Message = "Hello! it's " + DateTime.Now.ToUniversalTime().ToLongTimeString();
 		}	
 	}
 
@@ -44,17 +63,23 @@ namespace Zen.RPC.Common
 	public class GetACSResultPayload : ResultPayload
 	{
 		public ContractData[] Contracts { get; set; }	
+
+		public override string ToString()
+		{
+			var count = Contracts == null ? "none" : Contracts.Length.ToString();
+			return base.ToString() + $" {count} Contracts";
+		}
 	}
 
-	public class GetContractCodePayload : BasePayload
-	{
-		public byte[] Hash { get; set; }
-	}
+	//public class GetContractCodePayload : BasePayload
+	//{
+	//	public byte[] Hash { get; set; }
+	//}
 
-	public class GetContractCodeResultPayload : ResultPayload
-	{
-		public byte[] Code { get; set; }
-	}
+	//public class GetContractCodeResultPayload : ResultPayload
+	//{
+	//	public byte[] Code { get; set; }
+	//}
 
 	public class GetContractTotalAssetsPayload : BasePayload
 	{
@@ -67,38 +92,60 @@ namespace Zen.RPC.Common
 		public ulong Unconfirmed { get; set; }
 	}
 
-	public class GetOutpointPayload : BasePayload
+	public class GetContractPointedOutputsPayload : BasePayload
 	{
-		public byte[] Asset { get; set; }
-		public byte[] Address { get; set; }
-		public bool IsContract { get; set; }
-		public bool IsPK { get; set; }
+		public byte[] ContractHash { get; set; }
 	}
 
-	public class GetOutpointResultPayload : ResultPayload
+	public class GetContractPointedOutputsResultPayload : ResultPayload
 	{
-		public byte[] TXHash { get; set; }
-		public uint Index { get; set; }
-	}
+		public List<Tuple<byte[], byte[]>> PointedOutputs { get; set; }
 
-	public class GetPointedOutpointPayload : BasePayload
-	{
-		public byte[] Asset { get; set; }
-		public byte[] Address { get; set; }
-		public bool IsContract { get; set; }
-		public bool IsPK { get; set; }
-	}
+		public override string ToString()
+		{
+			var count = PointedOutputs == null ? "none" : PointedOutputs.Count().ToString();
+			return base.ToString() + $" {count} PointedOutputs";
+		}
 
-	public class GetPointedOutpointResultPayload : ResultPayload
-	{
-		public Consensus.Types.Output Output { get; set; }
-		public Consensus.Types.Outpoint Outpoint { get; set; }
+		static readonly MsgPack.Serialization.MessagePackSerializer<Types.Outpoint> outpointSerializer = 
+					Serialization.context.GetSerializer<Types.Outpoint>();
+		static readonly MsgPack.Serialization.MessagePackSerializer<Types.Output> outputSerializer =
+					Serialization.context.GetSerializer<Types.Output>();
+
+		public static List<Tuple<byte[], byte[]>> Pack(List<Tuple<Types.Outpoint, Types.Output>> list)
+		{
+			return new List<Tuple<byte[], byte[]>>(list.Select(
+				t => new Tuple<byte[], byte[]>(
+					outpointSerializer.PackSingleObject(t.Item1),
+					outputSerializer.PackSingleObject(t.Item2)
+				)));
+		}
+
+		public static IEnumerable<Tuple<Types.Outpoint, Types.Output>> Unpack(List<Tuple<byte[], byte[]>> list)
+		{
+            return list.Select(t => new Tuple<Types.Outpoint, Types.Output>(
+                outpointSerializer.UnpackSingleObject(t.Item1),
+                outputSerializer.UnpackSingleObject(t.Item2))
+            );
+        }
 	}
 
     public class SendContractPayload : BasePayload
 	{
 		public byte[] ContractHash { get; set; }
 		public byte[] Data { get; set; }
+	}
+
+	public class ActivateContractPayload : BasePayload
+	{
+		public string Code { get; set; }
+		public int Blocks { get; set; }
+	}
+
+	public class SpendPayload : BasePayload
+	{
+		public string Address { get; set; }
+		public ulong Amount { get; set; }
 	}
 
 	public class MakeTransactionPayload : BasePayload
