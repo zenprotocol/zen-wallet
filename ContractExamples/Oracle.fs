de9c3d6e066056586f2763ad8e1e340604427ced
Nathan Cook <nathan.cook@gmail.com>
2017-06-14 18:35:08 +0300

    underlying-price table from oracle

--- ContractExamples/Oracle.fs
+++ ContractExamples/Oracle.fs
@@ -10,13 +10,26 @@ let tickerSample =
     """{"underlying":"GOOG","price":123.12,"timestamp":12312312311}""" 
 type TickerJsonData = JsonProvider<tickerSample, SampleIsList=false>
 
+[<Literal>]
+let itemSample =
+    """{
+        "item":{"underlying":"GOOG","price":123.12,"timestamp":12312312311},
+        "nonce":"a32543452521452"
+        }""" 
+type ItemJsonData = JsonProvider<itemSample, SampleIsList=false>
+
 let commitments (items: TickerItem seq) (secret: byte[]) =
-    let toJson ({underlying=underlying;price=price;timestamp=timestamp} as item) =
-        TickerJsonData.Root(underlying,price,timestamp).JsonValue.ToString() |> System.Text.Encoding.ASCII.GetBytes
+    let jsonOfTickerItem ({underlying=underlying;price=price;timestamp=timestamp} as item) =
+        TickerJsonData.Root(underlying,price,timestamp).JsonValue
+    let serializedTickerItem item = jsonOfTickerItem(item).ToString() |> System.Text.Encoding.ASCII.GetBytes
     let nonce (bs:byte[]) = innerHash (Array.append bs secret)
+    let leaf (item:TickerItem) =
+        let itemJson = jsonOfTickerItem item
+        let nonceBytes = nonce (serializedTickerItem item)
+        JsonValue.Record [|("item", itemJson);("nonce", JsonValue.String <| System.Convert.ToBase64String nonceBytes) |]
     let leafData = [|
         for item in items ->
-            Array.append (toJson item) (nonce (toJson item))
+            (leaf item).ToString() |> System.Text.Encoding.ASCII.GetBytes
             |]
     let tree = Merkle.merkleTree leafData
     let auditPaths = seq {
@@ -35,3 +48,11 @@ let pathToContractData (path:Merkle.AuditPath) =
     use stream = new System.IO.MemoryStream()
     serializer.WriteObject(stream, path)
     stream.ToArray() |> System.Text.Encoding.ASCII.GetString
+
+let priceTable (m:Map<string,Merkle.AuditPath>) =
+    let price (bs:byte[]) =
+        let item = ItemJsonData.Parse(System.Text.Encoding.ASCII.GetString bs)
+        item.Item.Price
+    let s = Map.toList m
+    [ for (underlying, path) in s -> (underlying, price <| path.data)]
+
