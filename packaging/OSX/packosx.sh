9892e110cac20adb9dd802202bf47efb5bb0c4a7
Doron Somech <somdoron@Adams-MacBook.local>
2017-06-26 17:27:18 +0300

    package OSZ app to dmg file

---
+++ packaging/OSX/packosx.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+pwd=`pwd`
+cd "$(dirname "$0")"
+
+APP_NAME="Zen"
+
+rm -rf "$APP_NAME.app"
+mkdir "$APP_NAME.app"
+cd "$APP_NAME.app"
+
+mkdir Contents
+cd Contents
+
+cp ../../Info.plist .
+
+mkdir MacOS
+cd MacOS
+cp ../../../zen .
+cd ..
+
+mkdir Resources
+cd Resources
+
+ZENPATH=../../../../../Zen/bin/Debug
+
+cp $ZENPATH/*.dll ./
+cp $ZENPATH/zen.exe ./
+cp $ZENPATH/zen.exe.config ./
+cp ../../../Zen.icns ./
+cp $ZENPATH/*.json ./
+cp $ZENPATH/d3.min.js ./
+cp $ZENPATH/graph.html ./
+cp /usr/local/lib/libsodium.dylib ./
+
+cd ../../..
+
+rm -f Zen.dmg
+appdmg ./appdmg.json ./Zen.dmg
+
+cd "$pwd"
