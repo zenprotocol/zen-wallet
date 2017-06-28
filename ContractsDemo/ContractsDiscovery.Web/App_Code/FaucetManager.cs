4ca75778858927e9dd0a40d67705792ec36c3867
Sharon Urban <urban_sharon@hotmail.com>
2017-06-28 18:11:26 +0300

    merged faucet into explorer

---
+++ ContractsDemo/ContractsDiscovery.Web/App_Code/FaucetManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Web.Configuration;
+using Zen.RPC.Common;
+using Zen.RPC;
+using System.Linq;
+using Consensus;
+using System.Text;
+using System.Collections.Generic;
+using Wallet.core.Data;
+
+namespace ContractsDiscovery.Web
+{
+	public class FaucetManager
+	{
+		static string NodeRPCAddress = WebConfigurationManager.AppSettings["node"];
+		static string walletPrivateKey = WebConfigurationManager.AppSettings["walletPrivateKey"];
+
+        public string Message { get; private set; }
+		public bool IsSetup { get; private set; }
+
+		public FaucetManager()
+        {
+            if (!EnsureFunds())
+            {
+                Message = "No funds";
+                IsSetup = false;
+                return;
+            }
+
+            Message = "";
+			IsSetup = true;
+        }
+
+		bool EnsureFunds()
+		{
+			if (HasFunds())
+			{
+				return true;
+			}
+
+			if (!Acquire())
+			{
+				return false;
+			}
+
+			return HasFunds();
+		}
+
+		bool HasFunds()
+		{
+			var getBalanceResultPayload = Client.Send<GetBalanceResultPayload>(NodeRPCAddress, new GetBalancePayload() { Asset = Consensus.Tests.zhash }).Result;
+
+			if (!getBalanceResultPayload.Success)
+			{
+				return false;
+			}
+
+			return getBalanceResultPayload.Balance > 0;
+		}
+
+	    bool Acquire()
+		{
+			return Client.Send<ResultPayload>(NodeRPCAddress, new AcquirePayload() { PrivateKey = walletPrivateKey }).Result.Success;
+		}
+	}
+}
