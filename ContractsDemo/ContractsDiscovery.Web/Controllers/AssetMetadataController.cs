8bb4dc8d928f04019b7d9b581357c59be869d777
Sharon Urban <urban_sharon@hotmail.com>
2017-07-02 16:25:55 +0300

    reverted file deletion

--- ContractsDemo/ContractsDiscovery.Web/Controllers/AssetMetadataController.cs
+++ ContractsDemo/ContractsDiscovery.Web/Controllers/AssetMetadataController.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContractsDiscovery.Web.App_Data;
+using Newtonsoft.Json;
 
 namespace ContractsDiscovery.Web.Controllers
 {
-    public class AssetMetadataController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View ();
-        }
-    }
+	public class AssetMetadataController : Controller
+	{
+		public JsonResult Index(string id)
+		{
+			AssetMetadata assetMetaData = null;
+
+			var assetMetaDataFile = Path.Combine("db", "asset-metadata", $"{id}.json");
+			if (System.IO.File.Exists(assetMetaDataFile))
+			{
+				try
+				{
+					var json = System.IO.File.ReadAllText(assetMetaDataFile);
+					assetMetaData = JsonConvert.DeserializeObject<AssetMetadata>(json);
+				}
+				catch
+				{
+					assetMetaData = new AssetMetadata() { name = "Error" };
+				}
+			}
+			else
+			{
+				assetMetaData = new AssetMetadata();
+			}
+
+			return Json(assetMetaData, JsonRequestBehavior.AllowGet);
+		}
+	}
 }
