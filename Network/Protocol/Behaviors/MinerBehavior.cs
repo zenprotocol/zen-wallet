492a7ae6df05803c42c718f177232295c1cb09af
Sharon Urban <urban_sharon@hotmail.com>
2017-07-02 16:26:52 +0300

    bad fix revised

--- Network/Protocol/Behaviors/MinerBehavior.cs
+++ Network/Protocol/Behaviors/MinerBehavior.cs
@@ -230,7 +230,15 @@ namespace NBitcoin.Protocol.Behaviors
 			}
 			return result;
 		}
-			
+
+		void AttachedNode_StateChanged(Node node, NodeState oldState)
+		{
+			if (node.State == NodeState.HandShaked)
+			{
+				_BlockBroadcastHub.Nodes.TryAdd(node, node);
+			}
+		}
+
 		private void AnnounceAll()
 		{
 			foreach (var broadcasted in _HashToBlock)
@@ -270,6 +278,7 @@ namespace NBitcoin.Protocol.Behaviors
 		Timer _Flush;
 		protected override void AttachCore()
 		{
+            AttachedNode.StateChanged += AttachedNode_StateChanged;
 			AttachedNode.MessageReceived += AttachedNode_MessageReceived;
 			_Flush = new Timer(o =>
 			{
@@ -279,6 +288,7 @@ namespace NBitcoin.Protocol.Behaviors
 
 		protected override void DetachCore()
 		{
+            AttachedNode.StateChanged -= AttachedNode_StateChanged;
 			AttachedNode.MessageReceived -= AttachedNode_MessageReceived;
 
 			Node unused;
