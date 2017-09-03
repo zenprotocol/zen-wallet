45b75bfc7caa833f7be53bdf114bce6cb1ecd904
Sharon Urban <urban_sharon@hotmail.com>
2017-09-03 16:05:44 +0300

    added basic conversion functions

---
+++ ContractExamples/FStarCompatibility.fs
@@ -0,0 +1,16 @@
+﻿module ContractExamples.FStarCompatilibity
+
+let unCost (Zen.Cost.C inj:Zen.Cost.cost<'Aa, 'An>) : 'Aa = inj.Force()
+
+let vectorToList (z:Zen.Vector.t<'Aa, _>) : List<'Aa> =
+     // 0I's are eraseable
+     Zen.Vector.foldl 0I 0I (fun acc e -> Zen.Cost.ret (e::acc)) [] z 
+     |> unCost
+     |> List.rev
+
+
+let listToVector (ls:List<'Aa>) : Zen.Vector.t<'Aa, _> =
+    let len = List.length ls 
+    let lsIndexed = List.mapi (fun i elem -> bigint (len - i - 1), elem) ls // vertors are reverse-zero-indexed
+
+    List.foldBack (fun (i,x) acc -> Zen.Vector.VCons (i, x, acc)) lsIndexed Zen.Vector.VNil
---
+++ ContractExamples/Tests/FStarCompatilibity.fs
@@ -0,0 +1,11 @@
+﻿module Tests.ContractExamples.FStarCompatilibity
+
+open NUnit.Framework
+open ContractExamples.FStarCompatilibity
+
+let arr = [1;2;3]
+
+[<Test>]
+let ``Should get original value``() =
+    let vec = arr |> listToVector |> vectorToList
+    Assert.AreEqual (vec, arr)
