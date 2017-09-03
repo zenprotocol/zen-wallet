c3b9bf01bb767e660510bd4f6a51e3b34fe334d2
Sharon Urban <urban_sharon@hotmail.com>
2017-09-03 11:42:53 +0300

    refactoring and cleanup

--- Zen.Lib/extract.sh
+++ Zen.Lib/extract.sh
@@ -1,11 +1,11 @@
 ﻿#!/bin/bash
 
 # records hints
-../../externals/FStar/bin/fstar.exe --prims prims.fst --record_hints Consensus.Types.fst
-../../externals/FStar/bin/fstar.exe --prims prims.fst --record_hints Consensus.Realized.fst
+../../externals/FStar/bin/fstar.exe --prims prims.fst --record_hints Zen.Types.fst
+../../externals/FStar/bin/fstar.exe --prims prims.fst --record_hints Zen.Realized.fst
 
-# verification and extraction of FStar to FSharp code 
-../../externals/FStar/bin/fstar.exe --codegen FSharp --prims prims.fst --extract_module Consensus.Types --use_hints --n_cores 4 --odir fs Consensus.Types.fst
+# verification and extraction of FStar to FSharp code
+../../externals/FStar/bin/fstar.exe --codegen FSharp --prims prims.fst --extract_module Zen.Types --use_hints --n_cores 4 --odir fs Zen.Types.fst
 
 
 #TODO: rest source files
