9ee59a4afc5f4e2f092cd65a5864040ae83b08fa
Sharon Urban <urban_sharon@hotmail.com>
2017-10-26 16:05:35 +0300

    add oracle-data serialization and hashing function

--- Zulib/fsharp/Realized/Zen.Merkle.fs
+++ Zulib/fsharp/Realized/Zen.Merkle.fs
@@ -1,20 +1,39 @@
-#light "off"
 module Zen.Merkle
 
+open System
 open Zen.Types.Extracted
 
 module ZArr = Zen.Array.Extracted
 module Cost = Zen.Cost.Realized
+module sha3 = Zen.Sha3.Realized
 
-let private innerHash : byte[] -> byte[] =
-    fun bs ->
-    let res = Array.zeroCreate 32 in
-    let sha3 = new Org.BouncyCastle.Crypto.Digests.Sha3Digest(256) in
-    sha3.BlockUpdate(bs,0,Array.length bs);
-    sha3.DoFinal(res, 0) |> ignore;
-    res
+let private serialize = function
+    // Oracle data structure
+    | Data4 (_, _, _, _,
+      ByteArray (_, underlyingBytes),
+      UInt64 price,
+      UInt64 timestamp,
+      Hash nonce) ->
+        [ underlyingBytes; BitConverter.GetBytes price; BitConverter.GetBytes timestamp ]
+        |> List.fold (fun acc elem -> Array.append elem acc) [||]
+        |> Some
+    | _ -> None
 
-let rootFromAuditPath
+let getDataHash
+    ( _ : Prims.nat)
+    data
+    : Cost.t<hash option, Prims.unit> =
+        lazy (
+            match serialize data with
+            | None -> None
+            | Some bytes -> bytes |> sha3.get256 |> Some
+//TODO: use Option.bind
+//            serialize data
+//            |> Option.bind (fun bytes -> bytes |> sha3.get256 |> Some)
+        )
+        |> Cost.C
+
+let getRootFromAuditPath
     ( _: Prims.nat)
     ( item : hash )
     ( location: Prims.nat )
@@ -26,9 +45,9 @@ let rootFromAuditPath
                 (fun (v, loc) h ->
                     if loc % 2u = 0u
                     then
-                        (innerHash <| Array.append v h, loc >>> 1)
+                        (sha3.get256 <| Array.append v h, loc >>> 1)
                     else
-                        (innerHash <| Array.append h v, loc >>> 1))
+                        (sha3.get256 <| Array.append h v, loc >>> 1))
                 (item,uint32 location) hashes
         )
         |> Cost.C
--- Zulib/fstar/Zen.Merkle.fsti
+++ Zulib/fstar/Zen.Merkle.fsti
@@ -1,12 +1,19 @@
 module Zen.Merkle
 
 open Zen.Cost
+open Zen.Types.Extracted
+
 module  A = Zen.Array
 module  M = FStar.Mul
 module  C = Zen.Crypto
 
+val getDataHash:
+     #n:nat
+  -> data n
+  -> Cost.t (C.hash) n
 
-val rootFromAuditPath: #n:nat
+val getRootFromAuditPath:
+     #n:nat
   -> C.hash
   -> nat
   -> A.t C.hash n
