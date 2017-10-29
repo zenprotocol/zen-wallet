f56acdf4aa62cf540f574991d1d16e0647032c52
Sharon Urban <urban_sharon@hotmail.com>
2017-10-29 18:36:09 +0200

    Zen.FSharp compiler utility

--- ContractExamples/FStar/compile.sh
+++ ContractExamples/FStar/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fsharpc --mlcompatibility -a ZenModule.fs -r ../../Zulib/bin/Zulib.dll -r ../../tools/fstar/mono/FSharp.Compatibility.OCaml.dll
+mono ../../ZenFSharpC/bin/Debug/ZenFSharpC.exe --mlcompatibility -a ZenModule.fs -r ../../Zulib/bin/Zulib.dll -r ../../tools/fstar/mono/FSharp.Compatibility.OCaml.dll
